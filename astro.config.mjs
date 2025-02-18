4d045f6c1eb29f006464a2bd0f0b3db767069cfe
Mael Ferreira <maeferre@z3r2p6.42lyon.fr>
2025-02-18 22:16:16 +0100

    add photo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
+import sitemap from "@astrojs/sitemap";
+
+const defaultLocale = "en";
+const locales = 
+{
+    en: "en-US",
+    fr: "fr-FR"
+}
+
+export default defineConfig({
+    integrations: [
+      i18n({
+        defaultLocale: defaultLocale,
+        locales: locales, 
+      }),
+      sitemap(),
+    ],
+  });
 
 // https://astro.build/config
-export default defineConfig({});
+
